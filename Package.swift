ad486156cbc9446044d7da6f6c72133e5553f253
KC-2001MS <iroiro.work1234@gmail.com>
2026-07-12 23:57:18 +0900

    add actions

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         .executable(name: "sclt", targets: ["sclt"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
-        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.8.0"),
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.5.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
